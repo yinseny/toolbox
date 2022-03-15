c122828d1478c90d65636481a3b6fcfd80a653a1
Eason Yang <i@yins.me>
2022-03-15 23:29:13 +0800

    add recalculate-episode script

--- recalculate-episodes.mjs
+++ recalculate-episodes.mjs
@@ -6,10 +6,9 @@ import { basename, dirname, join } from 'path';
 program.version('0.0.1');
 
 program
-  .requiredOption('-s, --season <number>', 'season offset')
-  .requiredOption('-e, --episode <number>', 'episode offset')
+  .requiredOption('-e, --expression <expression>', 'new episode expression')
   .requiredOption('-f, --folder <folder>', 'folder')
-  .option('-t --target <folder>', 'target folder')
+  .option('-p, --pattern <pattern>', 'pattern')
   .option('-d --do', 'really do it');
 
 program.parse(process.argv);
@@ -17,7 +16,7 @@ program.parse(process.argv);
 const options = program.opts();
 
 console.log(`search all videos & subtitles in ${options.folder}`);
-let files = glob.sync('**/*.+(mkv|mp4|ass|srt|smi)', {
+let files = glob.sync(`**/${options.pattern || '*'}.+(mkv|mp4|ass|srt|smi)`, {
   absolute: true,
   cwd: options.folder,
 });
@@ -31,10 +30,10 @@ for (const file of files) {
   const [text, season, episode] = match;
   const index = match.index;
 
-  const newSeason = Number(season) + Number(options.season);
-  const newEpisode = Number(episode) + Number(options.episode);
-  const newName = name.substring(0, index) + `S${newSeason}E${newEpisode}` + name.substring(index + text.length);
-  const newFile = join(options.target || dir, newName);
+  const newSE = eval(`(() => {const s = ${Number(season)}, e = ${Number(episode)}; return ${options.expression};})()`);
+
+  const newName = name.substring(0, index) + newSE + name.substring(index + text.length);
+  const newFile = join(dir, newName);
   console.log('mv', file, newFile);
 
   if (options.do) {
